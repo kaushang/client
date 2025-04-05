46a4ebacd29e6c3af3288d288faf16ec1780cf61
Kaushang Surya <kaushangsurya29@gmail.com>
2025-04-05 20:02:31 +0530

    form validation added

--- src/landing/LogInForm.jsx
+++ src/landing/LogInForm.jsx
@@ -1,26 +1,135 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { useRef } from "react";
 import { useAuth } from "../AuthContext"; // Add this import
 
 function LogInForm({ switchForm }) {
   const navigate = useNavigate();
   const errorMessage = useRef(null);
+  const email = useRef(null);
+  const emailIcon = useRef(null);
+  const password = useRef(null);
+  const passwordIcon = useRef(null);
+  const submitBtn = useRef(null);
   const { login } = useAuth(); // Add this to access the login function
+
+  // keep tracks of each input fields in the form and stores the value of each field
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  // keep track of all the input fields and whether they are valid or not
+  const [validFields, setValidFields] = useState({
+    email: false,
+    password: false,
+  });
+
+  // Validate function: Checks whether each field is valid or not
+  const validate = (name, value) => {
+    errorMessage.current.textContent = "";
+
+    switch (name) {
+      case "email":
+        if (value.length === 0) {
+          email.current.style.border = "";
+          emailIcon.current.style.display = "none";
+          break;
+        }
+
+        const isValidEmail = /^\S+@\S+\.\S+$/.test(value);
+        email.current.style.border = isValidEmail ? "" : "2px solid red";
+        emailIcon.current.style.display = "block";
+
+        emailIcon.current.classList.toggle("icons-yes", isValidEmail);
+        emailIcon.current.classList.toggle("fa-circle-check", isValidEmail);
+        emailIcon.current.classList.toggle("icons-no", !isValidEmail);
+        emailIcon.current.classList.toggle(
+          "fa-circle-exclamation",
+          !isValidEmail
+        );
+
+        if (!isValidEmail) {
+          errorMessage.current.textContent = "Enter a valid email";
+        }
+        setValidFields((prev) => ({ ...prev, email: isValidEmail }));
+        break;
+
+      case "password":
+        if (value.length === 0) {
+          password.current.style.border = "";
+          passwordIcon.current.style.display = "none";
+          break;
+        }
+
+        const isValidPassword = value.length >= 6;
+        password.current.style.border = isValidPassword ? "" : "2px solid red";
+        passwordIcon.current.style.display = "block";
+
+        passwordIcon.current.classList.toggle("icons-yes", isValidPassword);
+        passwordIcon.current.classList.toggle(
+          "fa-circle-check",
+          isValidPassword
+        );
+        passwordIcon.current.classList.toggle("icons-no", !isValidPassword);
+        passwordIcon.current.classList.toggle(
+          "fa-circle-exclamation",
+          !isValidPassword
+        );
+
+        if (!isValidPassword) {
+          errorMessage.current.textContent =
+            "Password must be at least 6 characters";
+        }
+        setValidFields((prev) => ({ ...prev, password: isValidPassword }));
+        break;
+
+      default:
+        break;
+    }
+  };
+
+  // Handle changes of values: Updates values after every change
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+    validate(name, value);
+  };
+
+  // useEffect to check if all fields are filled and valid
+  useEffect(() => {
+    const allFieldsFilled = Object.values(formData).every(
+      (field) => field.trim() !== ""
+    );
+    const allFieldsValid = Object.values(validFields).every(
+      (isValid) => isValid
+    );
+
+    if (allFieldsValid && allFieldsFilled) {
+      submitBtn.current.removeAttribute("disabled");
+      submitBtn.current.classList.remove("disable");
+    } else {
+      submitBtn.current.setAttribute("disabled", "true");
+      submitBtn.current.classList.add("disable");
+    }
+  }, [formData, validFields]);
 
   async function check(event) {
     event.preventDefault();
 
     errorMessage.current.textContent = "";
+    
+    const url = "https://server-71hv.onrender.com/api/login";
+
     const data = {
-      email: document.querySelector('input[name="email"]').value,
-      password: document.querySelector('input[name="password"]').value,
+      email: formData.email,
+      password: formData.password,
     };
-    if (data.email === "" || data.password === "") {
-      return (errorMessage.current.textContent = "Please fill all the fields");
-    }
-    const response = await fetch("https://server-71hv.onrender.com/api/login", {
-    // const response = await fetch("/api/login", {
+
+    const response = await fetch(url, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(data),
@@ -40,15 +149,43 @@ function LogInForm({ switchForm }) {
   return (
     <form className="form-section-child" action="/login" method="POST">
       <h1 className="heading">Log In</h1>
+      <section className="form-group">
+        <input
+          ref={email}
+          required
+          placeholder="Email"
+          type="text"
+          name="email"
+          onChange={handleChange}
+        />
+        <i
+          ref={emailIcon}
+          style={{ display: "none" }}
+          className="icons-no fa-solid fa-circle-exclamation"
+        ></i>
+      </section>
 
-      <input required placeholder="Email" type="text" name="email" />
-
-      <input required placeholder="Password" type="password" name="password" />
+      <section className="form-group">
+        <input
+          ref={password}
+          required
+          placeholder="Password"
+          type="password"
+          name="password"
+          onChange={handleChange}
+        />
+        <i
+          ref={passwordIcon}
+          style={{ display: "none" }}
+          className="icons-no fa-solid fa-circle-exclamation"
+        ></i>
+      </section>
 
       <span ref={errorMessage} id="errorMessage"></span>
 
       <input
-        className="submit-form"
+        ref={submitBtn}
+        className="submit-form disable"
         onClick={check}
         id="submit"
         type="button"
--- src/landing/SignUpForm.jsx
+++ src/landing/SignUpForm.jsx
@@ -1,17 +1,23 @@
 import React from "react";
 import "../App.css";
 import { useNavigate } from "react-router-dom";
-import { useRef, useState } from "react";
+import { useRef, useState, useEffect } from "react";
 import { useAuth } from "../AuthContext"; // Add this import
 
 function SignUpForm({ switchForm }) {
   const navigate = useNavigate();
   const errorMessage = useRef(null);
   const email = useRef(null);
+  const emailIcon = useRef(null);
   const username = useRef(null);
+  const usernameIcon = useRef(null);
   const password = useRef(null);
+  const passwordIcon = useRef(null);
+  const nameIcon = useRef(null);
+  const submitBtn = useRef(null);
   const { login } = useAuth(); // Add this to access the login function
-  
+
+  // keep tracks of each input fields in the form and stores the value of each field
   const [formData, setFormData] = useState({
     email: "",
     username: "",
@@ -19,32 +25,122 @@ function SignUpForm({ switchForm }) {
     password: "",
   });
 
-  // const [errorMessage, setErrors] = useState({});
+  // keep track of all the input fields and whether they are valid or not
+  const [validFields, setValidFields] = useState({
+    name: false,
+    username: false,
+    email: false,
+    password: false,
+  });
 
-  // const validate = (name, value) => {
-  //   errorMessage.current.textContent = "";
+  // Validate function: Checks whether each field is valid or not
+  const validate = (name, value) => {
+    errorMessage.current.textContent = "";
 
-  //   switch (name) {
-  //     case "email":
-  //         if (!/^\S+@\S+\.\S+$/.test(value)) {
-  //           return (errorMessage.current.textContent = "Invalid email address");
-  //         }
-  //         break;
-  //     case "username":
-  //       if (value.length < 4) {
-  //         return (errorMessage.current.textContent = "Username must be at least 4 characters");
-  //       }
-  //       break;
-  //       case "password":
-  //         if (value.length < 6) {
-  //         return (errorMessage.current.textContent = "Password must be at least 6 characters");
-  //       }
-  //       break;
-  //     default:
-  //       break;
-  //   }
-  // };
+    switch (name) {
+      case "email":
+        if (value.length === 0) {
+          email.current.style.border = "";
+          emailIcon.current.style.display = "none";
+          break;
+        }
 
+        const isValidEmail = /^\S+@\S+\.\S+$/.test(value);
+        email.current.style.border = isValidEmail ? "" : "2px solid red";
+        emailIcon.current.style.display = "block";
+
+        emailIcon.current.classList.toggle("icons-yes", isValidEmail);
+        emailIcon.current.classList.toggle("fa-circle-check", isValidEmail);
+        emailIcon.current.classList.toggle("icons-no", !isValidEmail);
+        emailIcon.current.classList.toggle(
+          "fa-circle-exclamation",
+          !isValidEmail
+        );
+
+        if (!isValidEmail) {
+          errorMessage.current.textContent = "Enter a valid email";
+        }
+        setValidFields((prev) => ({ ...prev, email: isValidEmail }));
+        break;
+
+      case "username":
+        if (value.length === 0) {
+          username.current.style.border = "";
+          usernameIcon.current.style.display = "none";
+          break;
+        }
+
+        const isValidUsername = value.length >= 4;
+        username.current.style.border = isValidUsername ? "" : "2px solid red";
+        usernameIcon.current.style.display = "block";
+
+        usernameIcon.current.classList.toggle("icons-yes", isValidUsername);
+        usernameIcon.current.classList.toggle(
+          "fa-circle-check",
+          isValidUsername
+        );
+        usernameIcon.current.classList.toggle("icons-no", !isValidUsername);
+        usernameIcon.current.classList.toggle(
+          "fa-circle-exclamation",
+          !isValidUsername
+        );
+
+        if (!isValidUsername) {
+          errorMessage.current.textContent =
+            "Username must be at least 4 characters";
+        }
+        setValidFields((prev) => ({ ...prev, username: isValidUsername }));
+        break;
+
+      case "name":
+        if (value.length > 0) {
+          nameIcon.current.style.display = "block";
+          nameIcon.current.classList.remove(
+            "icons-no",
+            "fa-circle-exclamation"
+          );
+          nameIcon.current.classList.add("icons-yes", "fa-circle-check");
+        } else {
+          nameIcon.current.style.display = "none";
+        }
+        setValidFields((prev) => ({ ...prev, name: value.length > 0 }));
+        break;
+
+      case "password":
+        if (value.length === 0) {
+          password.current.style.border = "";
+          passwordIcon.current.style.display = "none";
+          break;
+        }
+
+        const isValidPassword = value.length >= 6;
+        password.current.style.border = isValidPassword ? "" : "2px solid red";
+        passwordIcon.current.style.display = "block";
+
+        passwordIcon.current.classList.toggle("icons-yes", isValidPassword);
+        passwordIcon.current.classList.toggle(
+          "fa-circle-check",
+          isValidPassword
+        );
+        passwordIcon.current.classList.toggle("icons-no", !isValidPassword);
+        passwordIcon.current.classList.toggle(
+          "fa-circle-exclamation",
+          !isValidPassword
+        );
+
+        if (!isValidPassword) {
+          errorMessage.current.textContent =
+            "Password must be at least 6 characters";
+        }
+        setValidFields((prev) => ({ ...prev, password: isValidPassword }));
+        break;
+
+      default:
+        break;
+    }
+  };
+
+  // Handle changes of values: Updates values after every change
   const handleChange = (event) => {
     const { name, value } = event.target;
 
@@ -52,33 +148,41 @@ function SignUpForm({ switchForm }) {
       ...prevData,
       [name]: value,
     }));
-    // validate(name, value);
+    validate(name, value);
   };
 
+  // useEffect: Checks whether all fields are filled and valid before enabling the submit button
+  useEffect(() => {
+    const allFieldsFilled = Object.values(formData).every(
+      (field) => field.trim() !== ""
+    );
+    const allFieldsValid = Object.values(validFields).every(
+      (isValid) => isValid
+    );
+
+    if (allFieldsFilled && allFieldsValid) {
+      submitBtn.current.removeAttribute("disabled");
+      submitBtn.current.classList.remove("disable");
+    } else {
+      submitBtn.current.setAttribute("disabled", "true");
+      submitBtn.current.classList.add("disable");
+    }
+  }, [formData, validFields]);
+
+  //Create account function: Executes when submit button is clicked
   async function createAcc(event) {
     event.preventDefault();
 
     errorMessage.current.textContent = "";
 
-    const submit = document.getElementById("submit");
-    submit.removeAttribute("disabled");
     const url = "https://server-71hv.onrender.com/api/create";
     // const url = "/api/create";
     const data = {
       email: formData.email,
       username: formData.username,
-      name: document.querySelector('input[name="name"]').value,
-      password: document.querySelector('input[name="password"]').value,
+      name: formData.username,
+      password: formData.password,
     };
-
-    if (
-      data.email === "" ||
-      data.username === "" ||
-      data.name === "" ||
-      data.password === ""
-    ) {
-      return (errorMessage.current.textContent = "Please fill all the fields");
-    }
 
     const response = await fetch(url, {
       method: "POST",
@@ -102,45 +206,76 @@ function SignUpForm({ switchForm }) {
   return (
     <form className="form-section-child" action="" method="POST">
       <h1 className="heading">Create an account</h1>
+      <section className="form-group">
+        <input
+          ref={email}
+          required
+          placeholder="Email"
+          type="text"
+          name="email"
+          onChange={handleChange}
+          value={formData.email}
+        />
+        <i
+          ref={emailIcon}
+          style={{ display: "none" }}
+          className="icons-no fa-solid fa-circle-exclamation"
+        ></i>
+      </section>
+      <section className="form-group">
+        <input
+          ref={username}
+          required
+          placeholder="Username"
+          type="text"
+          name="username"
+          onChange={handleChange}
+          value={formData.username}
+        />
+        <i
+          ref={usernameIcon}
+          style={{ display: "none" }}
+          className="icons-no fa-solid fa-circle-exclamation"
+        ></i>
+      </section>
+      <section className="form-group">
+        <input
+          required
+          placeholder="Name"
+          type="text"
+          name="name"
+          onChange={handleChange}
+          value={formData.name}
+        />
+        <i
+          ref={nameIcon}
+          style={{ display: "none" }}
+          className="icons-yes fa-solid fa-circle-check"
+        ></i>
+      </section>
 
-      <input
-      ref={email}
-        required
-        placeholder="Email"
-        type="text"
-        name="email"
-        onChange={handleChange}
-        value={formData.email}
-      />
-
-      <input
-      ref={username}
-        required
-        placeholder="Username"
-        type="text"
-        name="username"
-        onChange={handleChange}
-        value={formData.username}
-      />
-      {/* <i class="fa-solid fa-circle-exclamation"></i> */}
-      {/* <i class="fa-solid fa-circle-check"></i> */}
-
-      <input required placeholder="Name" type="text" name="name" onChange={handleChange} value={formData.name}/>
-
-      <input
-      ref={password}
-        required
-        placeholder="Password"
-        type="password"
-        name="password"
-        onChange={handleChange}
-        value={formData.password}
-      />
+      <section className="form-group">
+        <input
+          ref={password}
+          required
+          placeholder="Password"
+          type="password"
+          name="password"
+          onChange={handleChange}
+          value={formData.password}
+        />
+        <i
+          ref={passwordIcon}
+          style={{ display: "none" }}
+          className="icons-no fa-solid fa-circle-exclamation"
+        ></i>
+      </section>
 
       <span ref={errorMessage} id="errorMessage"></span>
 
       <input
-        className="submit-form"
+        ref={submitBtn}
+        className="disable submit-form"
         id="submit"
         onClick={createAcc}
         type="button"
